5a315a1f40a0bcd7f1281463ecb82c24a1321527
Mayank Phadke <mphadke1@hawk.iit.edu>
2021-05-06 00:17:47 -0500

    use HashRouter instead of BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import SearchPage from "./Pages/SearchPage"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class App extends React.Component {
     render() {
